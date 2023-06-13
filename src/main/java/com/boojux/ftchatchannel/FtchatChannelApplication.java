c8d7b6e04f5f4df878c269cd8e7cafd968420928
Boojux <iamboojux@gmail.com>
2023-06-13 15:00:31 +0800

    Initial commit

---
+++ src/main/java/com/boojux/ftchatchannel/FtchatChannelApplication.java
@@ -0,0 +1,13 @@
+package com.boojux.ftchatchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtchatChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtchatChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boojux/ftchatchannel/FtchatChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boojux.ftchatchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtchatChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
