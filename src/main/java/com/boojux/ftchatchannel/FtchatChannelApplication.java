ecbcd024b6d2661e561f9fec2026905cf2d6969c
Boojux <iamboojux@gmail.com>
2025-03-19 14:28:15 +0800

    add: enable scheduling, replace printException with logger.error()

--- src/main/java/com/boojux/ftchatchannel/FtchatChannelApplication.java
+++ src/main/java/com/boojux/ftchatchannel/FtchatChannelApplication.java
@@ -1,19 +1,24 @@
 package com.boojux.ftchatchannel;
 
 import com.boojux.ftchatchannel.server.NettyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class FtchatChannelApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(FtchatChannelApplication.class.getName());
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(FtchatChannelApplication.class, args);
 		try {
 			context.getBean(NettyServer.class).start();
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("Netty server start error: {} ", e.getMessage());
 		}
 	}
 
