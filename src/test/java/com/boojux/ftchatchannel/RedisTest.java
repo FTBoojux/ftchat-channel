c37a5caa4d0abb76082d6e1ccb571c235b1dd5f7
Boojux <iamboojux@gmail.com>
2025-03-19 14:42:11 +0800

    add: RedisTest.testSyncOfflineMessageToCassandra

--- src/test/java/com/boojux/ftchatchannel/RedisTest.java
+++ src/test/java/com/boojux/ftchatchannel/RedisTest.java
@@ -1,5 +1,6 @@
 package com.boojux.ftchatchannel;
 
+import com.boojux.ftchatchannel.syncTask.RedisToCassandraSyncTask;
 import com.boojux.ftchatchannel.utils.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,19 @@ import java.util.Set;
 public class RedisTest {
     @Autowired
     private RedisUtils redisUtils;
+    @Autowired
+    private RedisToCassandraSyncTask redisToCassandraSyncTask;
+
     @Test
     public void testScanKeys(){
         Set<String> strings = redisUtils.scanKeys("offline:message:*");
         assert strings != null;
         log.info(strings.toString());
     }
+
+    @Test
+    public void testSyncOfflineMessageToCassandra(){
+        redisToCassandraSyncTask.syncOfflineMessageToCassandra();
+    }
+
 }
